429142219e9084aa2d866098def733b7dae2b05d
jameschristou <james.christou@gmail.com>
2020-01-08 23:54:23 +1100

    Properly styling action buttons and using tick and cross svg

--- src/EditableTextfield.js
+++ src/EditableTextfield.js
@@ -63,8 +63,16 @@ const EditableTextfield = ({value, placeholder, updatedTextHandler}) => {
       </div>
       <div className={`editableTextfieldActionsContainer${showActionButtons ? '' : ' hidden'}`}>
         <div className="editableTextfieldActions">
-          <button className="editableTextfieldAction" onClick={evnt => onSaveHandler(evnt)}>+</button>
-          <button className="editableTextfieldAction" onClick={evnt => onCancelHandler(evnt)}>X</button>
+          <button className="editableTextfieldAction" onClick={evnt => onSaveHandler(evnt)}>
+            <svg width="24" height="24" viewBox="1 -2 24 24" focusable="false" role="presentation">
+              <path d="M6.735 12.322a1 1 0 0 0-1.47 1.356l3.612 3.919c.537.526 1.337.526 1.834.03l.364-.359a2335.638 2335.638 0 0 0 3.939-3.883l.04-.04a492.598 492.598 0 0 0 3.658-3.643 1 1 0 0 0-1.424-1.404 518.42 518.42 0 0 1-3.64 3.625l-.04.04a2049.114 2049.114 0 0 1-3.775 3.722l-3.098-3.363z"></path>
+            </svg>
+          </button>
+          <button className="editableTextfieldAction" onClick={evnt => onCancelHandler(evnt)}>
+            <svg width="24" height="24" viewBox="1 -2 24 24" focusable="false" role="presentation">
+              <path d="M12 10.586L6.707 5.293a1 1 0 0 0-1.414 1.414L10.586 12l-5.293 5.293a1 1 0 0 0 1.414 1.414L12 13.414l5.293 5.293a1 1 0 0 0 1.414-1.414L13.414 12l5.293-5.293a1 1 0 1 0-1.414-1.414L12 10.586z"></path>
+            </svg>
+          </button>
         </div>
       </div>
     </div>
